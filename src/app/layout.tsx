0544aa8770b4f8c13d598d7d10396098f0baf4f4
Faniry1015 <frandriaharimino@yahoo.com>
2024-10-13 13:29:29 +0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
