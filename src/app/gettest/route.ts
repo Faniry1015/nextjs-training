8d63697ddb5077b75d079d65c8ba5d8ce3181d28
Faniry1015 <frandriaharimino@yahoo.com>
2024-10-15 11:42:16 +0300

    post

--- src/app/gettest/route.ts
+++ src/app/gettest/route.ts
@@ -4,3 +4,19 @@ import { getData } from './data'
 export const GET = async () => {
     return Response.json(getData)
 }
+
+export const POST = async (request: Request) => {
+    const article = await request.json()
+    const newArticle = {
+        id: getData.length + 1,
+        label: article.label
+    }
+    getData.push(newArticle)
+
+    return new Response(JSON.stringify(newArticle), {
+        headers: {
+            'content-type': 'application/json'
+        },
+        status: 201
+    })
+}
