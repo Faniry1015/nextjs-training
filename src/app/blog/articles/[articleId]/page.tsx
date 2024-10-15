15c68a47c5e4d283ff2a40d6b8895e594f233c36
Faniry1015 <frandriaharimino@yahoo.com>
2024-10-15 07:08:20 +0300

    blog set without metadata

--- src/app/blog/articles/[articleId]/page.tsx
+++ src/app/blog/articles/[articleId]/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { articles } from "@/app/data";
+import Link from "next/link";
 
-const ArticleDetails = () => {
-  return (
-    <div>ArticleDetails</div>
-  )
-}
+const ArticleDetails = ({ params }: { params: { articleId: string } }) => {
+  const currentArticle = articles.find(
+    (article) => article.id === parseFloat(params.articleId)
+  );
+  console.log(currentArticle);
+  return <>
+    <h4 style={{margin: '0px'}}>Titre : {currentArticle?.title}</h4>
+    <h5 style={{margin: '0px'}}>Date : {currentArticle?.date}</h5>
+    <h5 style={{margin: '0px'}}>Auteur : {currentArticle?.author}</h5>
+    <p>{currentArticle?.description}</p>
+    <Link href='../articles'>Retour à la liste des articles</Link>
+  </>
+};
 
-export default ArticleDetails
+export default ArticleDetails;
