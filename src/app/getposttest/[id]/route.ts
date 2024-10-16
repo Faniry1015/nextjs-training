7888121abfcd05b0458a65f01aa4ad9e8112e8df
Faniry1015 <frandriaharimino@yahoo.com>
2024-10-16 07:13:47 +0300

    PATCH

--- src/app/getposttest/[id]/route.ts
+++ src/app/getposttest/[id]/route.ts
@@ -1,5 +1,14 @@
-import { getData } from "../data"
-export const GET = async (request: Request, { params }: {params: { id: string }})  => {
-    const article = getData.find((art) => art.id === parseInt(params.id))
-    return Response.json(article)
+import { getData } from "../data";
+export const GET = async (request: Request, {params}: {params: {id: string}}) => {
+    const item = getData.find( element => element.id === parseInt(params.id))
+    return Response.json(item)
+}
+
+export const PATCH = async (request: Request, {params} : {params: {id: string}}) => {
+    const res = await request.json()
+    const newLabel = res.label
+    const index = getData.findIndex(art => art.id === parseInt(params.id))
+    getData[index].label = newLabel
+    
+    return Response.json(getData[index])
 }
