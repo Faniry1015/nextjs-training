7fe8fd11c68f58f5915e43c213e702569431a717
Faniry1015 <frandriaharimino@yahoo.com>
2024-10-28 07:43:35 +0300

    patch

--- src/app/(getpost)/getpostroute/route.ts
+++ src/app/(getpost)/getpostroute/route.ts
@@ -1,35 +1,53 @@
-import React from 'react'
-import { getData } from './data'
-import { NextRequest } from 'next/server'
+import React from "react";
+import { getData } from "./data";
+import { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
-    const searchParam = request.nextUrl.searchParams
-    const query = searchParam.get('search')
-    const filteredData = query ? getData.find(art => art.label.includes(query)) : getData
-    return Response.json(filteredData)
-}
+  const searchParam = request.nextUrl.searchParams;
+  const query = searchParam.get("search");
+  const filteredData = query
+    ? getData.find((art) => art.label.includes(query))
+    : getData;
+  return Response.json(filteredData);
+};
 
 export const POST = async (request: Request) => {
-    const article = await request.json()
-    const newArticle = {
-        id: getData.length + 1,
-        label: article.label
-    }
-    getData.push(newArticle)
+  const article = await request.json();
+  const newArticle = {
+    id: getData.length + 1,
+    label: article.label,
+  };
+  getData.push(newArticle);
 
-    return new Response(JSON.stringify(newArticle), {
-        headers: {
-            'content-type': 'application/json'
-        },
-        status: 201
+  return new Response(JSON.stringify(newArticle), {
+    headers: {
+      "content-type": "application/json",
+    },
+    status: 201,
+  });
+};
+
+export const PATCH = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+  const body = await request.json();
+  const label = body.label;
+  const index = getData.findIndex(
+    (article) => article.id === parseInt(params.id)
+  );
+  getData[index].label = label;
+
+  return Response.json(getData[index]);
+};
+
+export const DELETE = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+    const index = getData.findIndex(article => {
+        article.id === parseInt(params.id)
     })
-}
-
-export const PATCH = async (request: Request, {params}: {params: {id: string}}) => {
-    const body = await request.json()
-    const label = body.label 
-    const index = getData.findIndex(article => article.id === parseInt(params.id)) 
-    getData[index].label = label
-
-    return Response.json(getData[index])
-}
+    getData.splice(index, 1)
+    return Response.json(getData)
+};
